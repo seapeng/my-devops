8964de25369feb77dfd4c85a1939be0059c17b50
Sea Peng <mr.seapeng@gmail.com>
2024-12-04 14:32:31 +0700

    fixed DevOps

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, World KKKK');
+  res.status(200).send('Hello, World SSSS');
 });
 
 app.get('/user/:id', (req, res) => {
