4d0c037021b22960ab3a06616557520bd79a51c8
Sea Peng <mr.seapeng@gmail.com>
2024-12-04 13:37:00 +0700

    fixed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, World KKKK 你好');
+  res.status(200).send('Hello, World KKKK');
 });
 
 app.get('/user/:id', (req, res) => {
