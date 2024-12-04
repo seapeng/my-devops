cf3a74a852915fc9263e45b225cf54b0ab548c5e
Sea Peng <mr.seapeng@gmail.com>
2024-12-04 15:00:21 +0700

    fixed DevOps

--- app.test.js
+++ app.test.js
@@ -7,7 +7,7 @@ describe('GET /', () => {
     it('should return Hello, World!', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toEqual(200);
-        expect(res.text).toBe('Hello, World SSSS');
+        expect(res.text).toBe('Hello, World 3333');
     });
 });
 
